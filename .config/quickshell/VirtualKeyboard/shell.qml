3d2c9f47d51aacd27f4d53c289741d724bd52199
Maskop <jachym.otahal@gmail.com>
2026-04-14 20:21:45 +0200

    some updates

--- .config/quickshell/VirtualKeyboard/shell.qml
+++
@@ -1,7 +0,0 @@
-import QtQuick
-import Quickshell
-
-ShellRoot {
-    id: shellRoot
-    Main{}
-}
--- .config/quickshell/VirtualKeyboard/virtual-keyboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if qs list --all | grep virtual-keyboard; then
-  qs -c ~/.config/quickshell/virtual-keyboard/ kill
-else
-  qs -c ~/.config/quickshell/virtual-keyboard/
-fi
